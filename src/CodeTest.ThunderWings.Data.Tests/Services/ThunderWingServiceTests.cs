733237f005561485dae1ae8fd5c4ecedfb64addf
CodeTile <43536260+CodeTile@users.noreply.github.com>
2024-07-31 09:55:28 +0100

    More ThunderWingServiceTests

--- src/CodeTest.ThunderWings.Data.Tests/Services/ThunderWingServiceTests.cs
+++ src/CodeTest.ThunderWings.Data.Tests/Services/ThunderWingServiceTests.cs
@@ -26,5 +26,57 @@ namespace CodeTest.ThunderWings.Data.Tests.Services
 			actual.CurrentPage.Should().Be(1);
 			actual.AsQueryable().Should().BeEquivalentTo(expected);
 		}
+
+		[TestMethod]
+		public void FindWithTopSpeedGtr1500()
+		{
+			// arrange
+			var expected = AircraftTestData.Aircraft09
+							.Where(m => m.TopSpeed >= 1500)
+							.AsQueryable();
+			var uot = new ThunderWingService(null);
+			uot._data = AircraftTestData.Aircraft09.AsQueryable();
+			var filter = new AircraftFilter
+			{
+				TopSpeed = new()
+				{
+					Min = 1500,
+				}
+			};
+			//act
+			var actual = uot.Find(filter);
+			//assert
+			actual.TotalCount.Should().Be(expected.Count());
+			actual.PageSize.Should().Be(10);
+			actual.TotalPages.Should().Be(1);
+			actual.CurrentPage.Should().Be(1);
+			actual.AsQueryable().Should().BeEquivalentTo(expected);
+		}
+
+		[TestMethod]
+		public void FindWithTopSpeedGtr1500PerPage3()
+		{
+			// arrange
+			var expected = AircraftTestData.Aircraft09
+							.Where(m => m.TopSpeed >= 1500)
+							.AsQueryable();
+			var uot = new ThunderWingService(null);
+			uot._data = AircraftTestData.Aircraft09.AsQueryable();
+			var filter = new AircraftFilter
+			{
+				TopSpeed = new()
+				{
+					Min = 1500,
+				},
+				PerPage = 3
+			};
+			//act
+			var actual = uot.Find(filter);
+			//assert
+			actual.TotalCount.Should().Be(expected.Count());
+			actual.PageSize.Should().Be(3);
+			actual.TotalPages.Should().Be(2);
+			actual.CurrentPage.Should().Be(1);
+		}
 	}
 }
